939fa99d5abc7359fd05cfca2db1c318309a0bf8
civilityin <info.civility@gmail.com>
2024-11-13 00:55:22 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// Main JavaScript File - Add interactive features if necessary
+
+// Example: Toggle menu for mobile view
+document.querySelector('.navbar').addEventListener('click', function () {
+  document.querySelector('.nav-links').classList.toggle('active');
+});

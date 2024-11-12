bb36cc1c9b9e75de14fad1ddb4119c3dcf60857e
civilityin <info.civility@gmail.com>
2024-11-13 02:02:33 +0530

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-// Main JavaScript File - Add interactive features if necessary
-
-// Example: Toggle menu for mobile view
-document.querySelector('.navbar').addEventListener('click', function () {
-  document.querySelector('.nav-links').classList.toggle('active');
-});
